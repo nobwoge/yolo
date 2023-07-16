b037f88d73f9461de96f95add0cc5237787d6b6d
nobwoge <alboh9@gmail.com>
2023-07-16 23:05:08 +0300

    add Terraform provider

--- main.tf
+++ main.tf
@@ -1,12 +1,9 @@
 # Configure the AWS Provider
 provider "aws" {
+  profile = "default"
   region = "us-east-1"
-  access_key = "AKIAWUYFUKGUESCYUJHH"
-  secret_key = "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"
 }
 resource "aws_instance" "yolo" {
   ami           = "ami-053b0d53c279acc90"
-  instance_type = "t3.micro"
-
-  
+  instance_type = "t3.micro" 
 }
